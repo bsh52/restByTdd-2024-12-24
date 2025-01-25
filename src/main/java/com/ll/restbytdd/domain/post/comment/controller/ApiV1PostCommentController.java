738bc26b53b55eb77a30e65df8c06fc5c8016682
paek1127 <paek1127@gmail.com>
2025-01-25 19:55:11 +0900

    feat: 댓글 삭제

--- src/main/java/com/ll/restbytdd/domain/post/comment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/ll/restbytdd/domain/post/comment/controller/ApiV1PostCommentController.java
@@ -1,14 +1,15 @@
 package com.ll.restbytdd.domain.post.comment.controller;
 
+import com.ll.restbytdd.domain.member.member.entity.Member;
 import com.ll.restbytdd.domain.post.comment.dto.PostCommentDto;
+import com.ll.restbytdd.domain.post.comment.entity.PostComment;
 import com.ll.restbytdd.domain.post.post.entity.Post;
 import com.ll.restbytdd.domain.post.post.service.PostService;
+import com.ll.restbytdd.global.exceptions.ServiceException;
 import com.ll.restbytdd.global.rq.Rq;
+import com.ll.restbytdd.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,11 +24,38 @@ public class ApiV1PostCommentController {
     public List<PostCommentDto> items(
             @PathVariable long postId
     ) {
-        Post post = postService.findById(postId).get();
+        Post post = postService.findById(postId).orElseThrow(
+                () -> new ServiceException("404-2", "%d번 글은 존재하지 않습니다.".formatted(postId))
+        );
 
         return post.getComments()
                 .stream()
                 .map(PostCommentDto::new)
                 .toList();
+    }
+
+    @DeleteMapping("/{commentId}")
+    public RsData<Void> delete(
+            @PathVariable long postId,
+            @PathVariable long commentId
+    ) {
+        Member actor = rq.checkAuthentication();
+
+        Post post = postService.findById(postId).orElseThrow(
+                () -> new ServiceException("404-2", "%d번 글은 존재하지 않습니다.".formatted(postId))
+        );
+
+        PostComment postComment = post.getCommentById(commentId).orElseThrow(
+                () -> new ServiceException("404-2", "%d번 댓글은 존재하지 않습니다.".formatted(commentId))
+        );
+
+        postComment.checkActorCanDelete(actor);
+
+        post.removeComment(postComment);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 댓글이 삭제되었습니다.".formatted(commentId)
+        );
     }
 }
